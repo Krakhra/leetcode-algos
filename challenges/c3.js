7f82114f5d6cf2aebe73fb210df2b0535ecc0b63
Kirat <hrakhra@mail.uoguelph.ca>
2019-05-09 11:08:43 -0400

    added challenge description

--- challenges/c3.js
+++ challenges/c3.js
@@ -1,3 +1,14 @@
+// challenge summary 
+/*
+We are given two strings, A and B.
+A shift on A consists of taking string A and moving
+the leftmost character to the rightmost position. 
+For example, if A = 'abcde', then it will be 'bcdea' 
+after one shift on A. Return True if and only if A can 
+become B after some number of shifts on A.
+*/
+
+
 function run(a,b){
   var len_a = a.length;
   var i=0;
@@ -18,3 +29,4 @@ function run(a,b){
 
 var out = run("abcde", "abced");
 console.log(out);
+
