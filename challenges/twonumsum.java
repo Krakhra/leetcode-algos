71d4be62b215ba361e94d66a16dc75ab96a3bf6f
Krakhra <31333868+Krakhra@users.noreply.github.com>
2020-05-22 08:53:10 -0400

    solution 2 for twoNumbersum

--- challenges/twonumsum.java
+++ challenges/twonumsum.java
@@ -22,3 +22,29 @@
     return new int[0];
   }
 }
+
+//Solution 2 O(nlog(n)) Time complexity O(1) Space  
+class Solution2 {
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+  	Arrays.sort(array);
+		
+    int left = 0;
+    int right = array.length-1;
+    
+    while(left < right){
+        int currentSum = array[left] + array[right];
+        
+        if(currentSum == targetSum){
+            return new int[] {array[left], array[right]};
+        }
+        else if(currentSum > targetSum){
+            right--;
+        }
+        else{
+            left++;
+        }
+            
+    }
+    return new int[0];
+  }
+}
