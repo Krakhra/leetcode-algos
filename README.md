46f3c2cf55b09500e0e9abfe954de1337f1bb1c2
Krakhra <31333868+Krakhra@users.noreply.github.com>
2020-05-22 09:26:17 -0400

    solution 1 of valid subsequence

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # leetcode-algos-python_learning
+Notion Page for tracking progress:https://www.notion.so/Leetcode-Tracking-1f3a1600f4184065ba42ef1b5a3b8c98
---
+++ challenges/validSubsequence.java
@@ -0,0 +1,25 @@
+/* 
+    Given two non=empty arrays of integers  write a function that determines whether the 
+    second array is a valid subsequence of the first one.
+*/
+
+import java.util.*;
+
+class Program {
+  public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
+    int aidx = 0;
+		int sidx = 0;
+		
+		while(aidx < array.size() && sidx < sequence.size()){
+			if(array.get(aidx) == sequence.get(sidx)){
+				sidx++;
+				aidx++;
+			}
+			else{
+				aidx++;
+			}
+		}
+		if(sidx == sequence.size()){return true;}
+    return false;
+  }
+}
