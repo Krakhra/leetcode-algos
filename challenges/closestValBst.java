416b6a12134e0c267c57d251cee31e1428098b7b
Krakhra <31333868+Krakhra@users.noreply.github.com>
2020-05-23 10:25:57 -0400

    finished closestval in bst problem

---
+++ challenges/closestValBst.java
@@ -0,0 +1,37 @@
+/*
+    Find Closest Value In BST
+    Write a function that takes in a binary search tree and a target integer value and returns
+    the closest value to the target in the bst
+ */
+
+class Program {
+  public static int findClosestValueInBst(BST tree, int target) {
+    return findClosestValueInBst(tree, target, Integer.MAX_VALUE);
+  }
+	
+	public static int findClosestValueInBst(BST tree, int target, int closest){
+		if(Math.abs(target - closest) > Math.abs(target - tree.value)){
+			closest = tree.value;
+		}
+		
+		if(target > tree.value && tree.right != null){
+			return findClosestValueInBst(tree.right, target, closest);
+		}
+		else if(target < tree.value && tree.left != null){
+			return findClosestValueInBst(tree.left, target, closest);
+		}
+		else{
+			return closest;
+		}
+	}
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
--- challenges/validSubsequence.java
+++ challenges/validSubsequence.java
@@ -4,22 +4,22 @@
 */
 
 import java.util.*;
-
+// O(n) time complexity O(1) Space
 class Program {
   public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
     int aidx = 0;
-		int sidx = 0;
+	int sidx = 0;
 		
-		while(aidx < array.size() && sidx < sequence.size()){
-			if(array.get(aidx) == sequence.get(sidx)){
-				sidx++;
-				aidx++;
-			}
-			else{
-				aidx++;
-			}
-		}
-		if(sidx == sequence.size()){return true;}
+    while(aidx < array.size() && sidx < sequence.size()){
+        if(array.get(aidx) == sequence.get(sidx)){
+            sidx++;
+            aidx++;
+        }
+        else{
+            aidx++;
+        }
+    }
+    if(sidx == sequence.size()){return true;}
     return false;
   }
 }
